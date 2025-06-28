d63bf6bd6c2385001ac6635d3a77cec5ecfb635b
curtasdeveloper <louisecurtandrei.pacete@gmail.com>
2025-06-28 22:51:15 +0800

    Fixed UI errors

--- src/MainHeader.jsx
+++ src/MainHeader.jsx
@@ -7,10 +7,10 @@ function Header() {
         <>
             <header className='flex items-center justify-center h-[20vh] w-[100%] bg-zinc-200 z-10 fixed top-0  left-0 right-0 shadow-sm'>
                 <nav className='flex gap-10' id='nav-container'>
-                    <a className='text-3xl inconsolata font-semilight' href="">Home</a>
-                    <a className='text-3xl inconsolata font-semilight' href="">About</a>
-                    <a className='text-3xl inconsolata font-semilight' href="">Features</a>
-                    <a className='text-3xl inconsolata font-semilight' href="">Demo</a>
+                    <a className='text-2xl inconsolata font-semilight' href="">Home</a>
+                    <a className='text-2xl inconsolata font-semilight' href="">About</a>
+                    <a className='text-2xl inconsolata font-semilight' href="">Features</a>
+                    <a className='text-2xl inconsolata font-semilight' href="">Demo</a>
                 </nav>
             </header>
         </>
--- src/Suggestion.jsx
+++ src/Suggestion.jsx
@@ -1,14 +1,18 @@
 import './index.css'
 
 function SuggestionForm () {
-    return <form className='flex flex-col items-start justify-center gap-4 w-[100%]' action="" method="post">
-        <label className='text-lg text-zinc-800 inconsolata' htmlFor="suggestion-text">Your Suggestion *</label>
-        <input className='border-1 border-zinc-400 font-monospace w-[100%] h-[48px] px-[16px] rounded-lg' type="text" id='suggestion-text' placeholder='Tell me about your idea, future request, or improvement suggestion...'/>
+    return <form className='flex flex-col items-start justify-center gap-4 w-[100%] py-[16px]' action="" method="post">
+        <div className='w-[100%]'>
+            <label className='text-md text-zinc-800 inconsolata' htmlFor="suggestion-text">Your Suggestion *</label>
+            <input className='border-1 text-sm border-zinc-400 font-monospace w-[100%] h-[32px] px-[8px] rounded-lg focus:outline-zinc-500 focus:bg-none focus:outline-1 focus:shadow-sm' type="text" id='suggestion-text' placeholder='Tell me about your idea, future request, or improvement suggestion...'/>
+        </div>
 
-        <label className='text-lg text-zinc-800 inconsolata' htmlFor="suggestion-name-or-email">Your Name or Email (Optional)</label>
-        <input className='border-1 border-zinc-400 font-monospace w-[100%] h-[48px] px-[16px] rounded-lg' type="text" id='suggestion-name-or-email' placeholder='john.doe@example or John Doe'/>
-        <span className='text-md text-zinc-700 inconsolata' >Leave your contact info if you'd like me to follow up with you.</span>
-        <button className='h-[48px] w-[148px] bg-zinc-900 text-zinc-100 rounded-[4px] inconsolata' type="submit">send suggestion.</button>
+        <div className='w-[100%]'>
+            <label className='text-md text-zinc-800 inconsolata' htmlFor="suggestion-name-or-email">Your Name or Email (Optional)</label>
+            <input className='border-1 text-sm border-zinc-400 font-monospace w-[100%] h-[32px] px-[8px] rounded-lg focus:outline-zinc-500 focus:bg-none focus:outline-1 focus:shadow-sm' type="text" id='suggestion-name-or-email' placeholder='john.doe@example or John Doe'/>
+            <span className='block text-sm text-zinc-700 inconsolata' >Leave your contact info if you'd like me to follow up with you.</span>
+        </div>
+        <button className='h-[40px] w-[148px] bg-zinc-900 text-zinc-100 rounded-[4px] inconsolata' type="submit">send suggestion.</button>
     </form>
 }
 function Suggestion() {
@@ -17,12 +21,12 @@ function Suggestion() {
             <div className='flex items-center justify-center h-[100vh] bg-zinc-200 '>
                 <main className='h-[80%] w-[50%] flex flex-col items-start justify-between bg-zinc-300 px-[2em] py-[3em] rounded-lg shadow-xl'>
                     <header>
-                        <h1 className='inconsolata text-3xl font-semibold text-zinc-900'>Help Me Improve</h1>
-                        <p className='text-xl text-zinc-800 inconsolata'>Have an idea for a new feature or improvement? I'd love to hear from you! Your feedback will helps me build a better experience for everyone.</p>
+                        <h1 className='inconsolata text-2xl font-semibold text-zinc-900'>Help Me Improve</h1>
+                        <p className='text-md text-zinc-800 inconsolata'>Have an idea for a new feature or improvement? I'd love to hear from you! Your feedback will helps me build a better experience for everyone.</p>
                     </header>
                     <SuggestionForm />
                     <footer className='flex justify-center w-[100%]'>
-                        <p className='text-center text-md text-zinc-800 inconsolata'>We review all suggestions and appreciate your time in helping us improve our platform.</p>
+                        <p className='text-center text-sm text-zinc-800 inconsolata'>We review all suggestions and appreciate your time in helping us improve our platform.</p>
                     </footer>
                 </main> 
             </div>
