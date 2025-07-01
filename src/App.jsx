3f22e2e225261f5fa29cb490a2d37a7138fe9474
curtasdeveloper <louisecurtandrei.pacete@gmail.com>
2025-07-01 23:21:11 +0800

    React Project Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import './index.css'
-import MainHeader from './MainHeader.jsx'
-import HomeMainContent from './HomeMainContent.jsx'
-import About from './About.jsx'
-import Features from './Features.jsx'
-import Suggestion from './Suggestion.jsx'
-import MainFooter from './MainFooter.jsx'
-import ProfileForm from './ProfileForm.jsx'
+
 
 
 function App() {
 
   return (
     <>
-      <MainHeader />
-      <HomeMainContent />
-      <About />
-      <Features />
-      <Suggestion />
-      <MainFooter />
-      <ProfileForm />
+
     </>
   )
 }
